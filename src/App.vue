59ee334c3a5658dff9bca7bb54c58a8f62d48209
Mihray <2636853047@qq.com>
2022-11-02 15:04:29 +0800

    fix：创建了新的项目

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,41 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <!-- <a href="#/">HomeVue</a>|
+  <a href="#/helloworld">HelloWorld</a> |
+  <a href="#/non-existent-path">Broken Link</a>|
+  <component :is="currentView" /> -->
+  <router-view></router-view>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import HomeVue from './components/HomeVue.vue'
+import NotFound from './components/NotFound.vue'
+
+const routes = {
+  '/': HomeVue,
+  '/helloworld': HelloWorld
+}
 
 export default {
   name: 'App',
   components: {
     HelloWorld
-  }
+  },
+  data() {
+    return {
+      currentPath:window.location.hash
+    }
+  },
+  computed: {
+    currentView() {
+      return routes[this.currentPath.slice(1)||'/']|| NotFound
+    }
+  },
+  mounted(){
+        window.addEventListener('hashchange',()=>{
+            this.currentPath=window.location.hash
+        })
+    },
 }
 </script>
 
